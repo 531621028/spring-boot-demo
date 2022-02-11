4d03bf515f14763edba1085504243215fdee2b8d
kang <531621028@qq.com>
2022-02-11 15:09:57 +0800

    EasyExcel

---
+++ src/test/java/com/hkk/demo/EasyExcelTest.java
@@ -0,0 +1,97 @@
+package com.hkk.demo;
+
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.ExcelWriter;
+import com.alibaba.excel.annotation.ExcelIgnore;
+import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.metadata.Head;
+import com.alibaba.excel.metadata.data.WriteCellData;
+import com.alibaba.excel.write.handler.CellWriteHandler;
+import com.alibaba.excel.write.metadata.WriteSheet;
+import com.alibaba.excel.write.metadata.holder.WriteSheetHolder;
+import com.alibaba.excel.write.metadata.holder.WriteTableHolder;
+import com.hkk.demo.utils.JsonUtil;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.usermodel.Cell;
+import org.junit.Test;
+
+/**
+ * @author kang
+ * @date 2022/2/11
+ */
+@Slf4j
+public class EasyExcelTest {
+
+    /**
+     * 最简单的写
+     * <p>1. 创建excel对应的实体对象 参照{@link DemoData}
+     * <p>2. 直接写即可
+     */
+    @Test
+    public void simpleWrite() {
+        // 写法1
+        String fileName = getPath() + "simpleWrite" + System.currentTimeMillis() + ".xlsx";
+        // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
+        // 如果这里想使用03 则 传入excelType参数即可
+        EasyExcel.write(fileName, DemoData.class).sheet("模板").doWrite(data());
+
+        // 写法2
+        fileName = getPath() + "simpleWrite" + System.currentTimeMillis() + ".xlsx";
+        // 这里 需要指定写用哪个class去写
+        ExcelWriter excelWriter = EasyExcel.write(fileName, DemoData.class).build();
+        WriteSheet writeSheet = EasyExcel.writerSheet("模板")
+            .registerWriteHandler(new MyCellWriteHandler()).build();
+        excelWriter.write(data(), writeSheet);
+        // 千万别忘记finish 会帮忙关闭流
+        excelWriter.finish();
+    }
+
+    public static String getPath() {
+        return Objects.requireNonNull(EasyExcelTest.class.getResource("/")).getPath();
+    }
+
+    private static List<DemoData> data() {
+        List<DemoData> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            DemoData data = new DemoData();
+            data.setString("字符串" + i);
+            data.setDate(new Date());
+            data.setDoubleData(0.56);
+            list.add(data);
+        }
+        return list;
+    }
+
+    private static class MyCellWriteHandler implements CellWriteHandler {
+
+        @Override
+        public void afterCellDispose(WriteSheetHolder writeSheetHolder,
+            WriteTableHolder writeTableHolder, List<WriteCellData<?>> cellDataList, Cell cell,
+            Head head, Integer relativeRowIndex, Boolean isHead) {
+            log.info(JsonUtil.toJsonStringOrEmpty(cellDataList));
+        }
+    }
+
+
+    @Data
+    private static class DemoData {
+
+        @ExcelProperty("字符串标题")
+        private String string;
+        @ExcelProperty("日期标题")
+        private Date date;
+        @ExcelProperty("数字标题")
+        private Double doubleData;
+        /**
+         * 忽略这个字段
+         */
+        @ExcelIgnore
+        private String ignore;
+    }
+
+}
