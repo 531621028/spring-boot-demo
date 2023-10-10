01e2b0fe9cbc85514f696bcc0509ba69e20e8213
kang <531621028@qq.com>
2023-10-10 16:19:34 +0800

    ohter kt

---
+++ src/main/java/com/hkk/demo/kt/QuickSort.java
@@ -0,0 +1,44 @@
+package com.hkk.demo.kt;
+
+public class QuickSort {
+
+    public int[] sortArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return nums;
+        }
+        quickSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    void quickSort(int[] nums, int start, int end) {
+        if (start < end) {
+            int middleIndex = partition(nums, start, end);
+            quickSort(nums, 0, middleIndex - 1);
+            quickSort(nums, middleIndex + 1, end);
+        }
+    }
+
+    int partition(int[] nums, int start, int end) {
+        // 相当于使用end作为枢纽字段
+        int i = start, j = start;
+        // i 之前的值都是小于枢纽的值
+        // j 向右遍历，将小于枢纽的值移到前面
+        while (j < end) {
+            if (nums[j] < nums[end]) {
+                swap(nums, i++, j);
+            }
+            j++;
+        }
+        swap(nums, i, end);
+        return i;
+    }
+
+    void swap(int[] nums, int i, int j) {
+        if (i == j) {
+            return;
+        }
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
+}
--- src/test/java/com/hkk/kt/QuickSortTest.java
+++ src/test/java/com/hkk/kt/QuickSortTest.java
@@ -1,6 +1,7 @@
 package com.hkk.kt;
 
 import com.google.common.base.Joiner;
+import com.hkk.demo.kt.QuickSort;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 import org.junit.Assert;
@@ -13,7 +14,7 @@ import org.junit.runners.JUnit4;
  * @date 2023/10/9
  */
 @RunWith(JUnit4.class)
-public class QuickSort {
+public class QuickSortTest {
 
     final static QuickSort quickSort = new QuickSort();
 
@@ -81,45 +82,5 @@ public class QuickSort {
         };
         Assert.assertEquals("-5,-2,3",
             Joiner.on(",").join(Arrays.stream(quickSort.sortArray(nums)).boxed().collect(Collectors.toList())));
-    }
-
-    public int[] sortArray(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return nums;
-        }
-        quickSort(nums, 0, nums.length - 1);
-        return nums;
-    }
-
-    void quickSort(int[] nums, int start, int end) {
-        if (start < end) {
-            int middleIndex = partition(nums, start, end);
-            quickSort(nums, 0, middleIndex - 1);
-            quickSort(nums, middleIndex + 1, end);
-        }
-    }
-
-    int partition(int[] nums, int start, int end) {
-        // 相当于使用end作为枢纽字段
-        int i = start, j = start;
-        // i 之前的值都是小于枢纽的值
-        // j 向右遍历，将小于枢纽的值移到前面
-        while (j < end) {
-            if (nums[j] < nums[end]) {
-                swap(nums, i++, j);
-            }
-            j++;
-        }
-        swap(nums, i, end);
-        return i;
-    }
-
-    void swap(int[] nums, int i, int j) {
-        if (i == j) {
-            return;
-        }
-        int t = nums[i];
-        nums[i] = nums[j];
-        nums[j] = t;
     }
 }
