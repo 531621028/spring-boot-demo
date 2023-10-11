1a8b9b2d0e1758acb753b5845839080743185295
kang <531621028@qq.com>
2023-10-11 17:20:46 +0800

    去掉无用的导入

--- src/test/java/com/hkk/demo/mapper/GoodsMapperTest.java
+++ src/test/java/com/hkk/demo/mapper/GoodsMapperTest.java
@@ -2,7 +2,6 @@ package com.hkk.demo.mapper;
 
 import com.baomidou.mybatisplus.test.autoconfigure.MybatisPlusTest;
 import com.hkk.demo.dto.Goods;
-import com.hkk.demo.mapper.GoodsMapper;
 import java.math.BigDecimal;
 import org.junit.Assert;
 import org.junit.Test;
